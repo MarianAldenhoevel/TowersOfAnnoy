3b043e1dddb45ac17e6f848be2697487efc586f6
MarianAldenhoevel <marian.aldenhoevel@marian-aldenhoevel.de>
2021-05-27 12:25:17 +0200

    Added rude noises

--- collect_samples.py
+++ collect_samples.py
@@ -1,20 +1,13 @@
+# A throw-away Python script to generate sample catalog data.
+
 import os
 import shutil
 
-print(  'const sample_catalog = new Map([ \n' \
-    '[\'Percussion\', [\n' \
-    '    [ \'hihat-808\',   \'hihat-808.wav\',   \'C\' ],\n' \
-    '    [ \'clap-808\',    \'clap-808.wav\',    \'D\' ],\n' \
-    '    [ \'kick-plain\',  \'kick-plain.wav\',  \'E\' ],\n' \
-    '    [ \'kick-808\',    \'kick-808.wav\',    \'F\' ],\n' \
-    '    [ \'snare-808\',   \'snare-808.wav\',   \'G\' ],\n' \
-    '    [ \'openhat-808\', \'openhat-808.wav\', \'A\' ],\n' \
-    '    [ \'top-808\',     \'tom-808.wav\',     \'B\' ],\n' \
-    ']],')
+# basenotes = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']
 
-rootdir = './_samples/MusyngKite/'
+# No flats:
+basenotes = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
 
-basenotes = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']
 octavemap = [
     ',,,,', 
     ',,,,,', 
@@ -24,12 +17,15 @@ octavemap = [
     ',',
     '',
     '',
-    '\''
+    '\'',
+    '\'\'',
+    '\'\'\''
 ]
 
 note2abc = {}
+abcnotes = []
 
-for octave in range(8):
+for octave in range(10):
     for n in basenotes:
         abcn = n.replace('b', '_');
         if (octave <= 6):
@@ -38,6 +34,32 @@ for octave in range(8):
             abcn = abcn.lower()
         
         note2abc[n + str(octave)] = abcn + octavemap[octave]
+        abcnotes.append(abcn + octavemap[octave])
+
+print('const sample_catalog = new Map([ \n')
+
+rootdir = './_samples/farts/'
+
+print('[\'Farts\', [')
+            
+noteindex = 5
+for _, _, files in os.walk(rootdir):
+    for file in files:
+        abcnote = abcnotes[noteindex]
+        noteindex += 1
+        s = '  [ ' + ('\'' + file.replace('.wav','') + '\',').ljust(13) + ' ' + ('\'' + file + '\',').ljust(11) + '  ' + ('\'' + abcnote + '\'').ljust(9) + ' ]'
+        if file != files[-1]:
+            s += ','
+
+        print(s)
+        
+        src = rootdir + '/' + file
+        dst = rootdir + '../../samples/' + file
+        shutil.copyfile(src, dst)
+        
+
+'''
+rootdir = './_samples/MusyngKite/'
 
 for instrumentpath, _, files in os.walk(rootdir):
   
@@ -66,3 +88,4 @@ for instrumentpath, _, files in os.walk(rootdir):
             print(']],')
 
 print(']);')
+'''
